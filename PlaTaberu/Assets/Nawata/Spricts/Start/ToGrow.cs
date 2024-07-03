dff27bb3aac0eb61137a8abe0e79b1b077b0998a
Karotte3500500 <karotte250500@gmail.com>
2024-07-03 18:24:03 +0900

    縄田
    初期設定

---
+++ PlaTaberu/Assets/Nawata/Spricts/Start/ToGrow.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ToGrow : MonoBehaviour
+{
+    public void LoadToGrow()
+    {
+        SceneManager.LoadScene("Grow");
+    }
+}

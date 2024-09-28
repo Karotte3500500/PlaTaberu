117b69b0b8cd0a71bc01c431fe14d26511cfedb9
kura314 <152850713+kura314@users.noreply.github.com>
2024-09-28 10:07:55 +0900

    kuramoto

--- PlaTaberu/Assets/Kuramoto/Spricts/send_file.cs
+++ PlaTaberu/Assets/Kuramoto/Spricts/send_file.cs
@@ -1,8 +1,10 @@
 using System.Collections;
 using System.IO;
+using System;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.Networking;
+using System.Net.Sockets;
 
 public class send_file : MonoBehaviour
 {
@@ -16,13 +18,12 @@ public class send_file : MonoBehaviour
 
     private string filePath; // アップロードしたいファイルのパスを指定
     private string url = "http://192.168.11.3/upload.php"; // サーバーのURLを指定
-
-    private test_rec testR;
+    // サーバー設定
+    private string host = "192.168.11.3";  // サーバーのIPアドレス
+    private int port = 5001;                         // サーバーと同じポート番号
 
     void Start()
     {
-        testR = FindObjectOfType<test_rec>();
-
         controlUI = FindObjectOfType<ControlUI>();
         filePath = Application.persistentDataPath + @"/texample.xml";
 
@@ -50,7 +51,7 @@ public class send_file : MonoBehaviour
                 Debug.Log("ファイルのアップロードに成功しました！");
                 Debug.Log(www.downloadHandler.text);
 
-                testR.ReceiveFile();
+                ReceiveFile();
             }
             else
             {
@@ -68,6 +69,57 @@ public class send_file : MonoBehaviour
             string xmlContent = "<root>\n\t<example>Sample Data</example>\n</root>";
             File.WriteAllText(path, xmlContent);
             Debug.Log("File created: " + path);
+        }
+    }
+
+    public void ReceiveFile()
+    {
+        try
+        {
+            // ソケットのセットアップ
+            TcpClient client = new TcpClient(host, port);
+            NetworkStream stream = client.GetStream();
+
+            // ファイルを保存するパス
+            string filePath = Path.Combine(Application.persistentDataPath, "received_file.xml");
+
+            if (!File.Exists(filePath))
+            {
+                using (FileStream fs = File.Create(filePath)) ;
+
+                /*
+                string xmlContent = "<root>\n\t<example>Sample Data</example>\n</root>";
+                File.WriteAllText(filePath, xmlContent);
+                Debug.Log("File created: " + filePath);
+                */
+            }
+
+            // 受信したデータをファイルに書き込み
+            using (FileStream fileStream = new FileStream(filePath, FileMode.Create, FileAccess.Write))
+            {
+                byte[] buffer = new byte[1024];
+                int bytesRead;
+
+                // データを受信し、バッファに書き込む
+                while ((bytesRead = stream.Read(buffer, 0, buffer.Length)) > 0)
+                {
+                    fileStream.Write(buffer, 0, bytesRead);
+                }
+            }
+
+            Debug.Log("ファイル受信が完了しました: " + filePath);
+            //デバッグ後削除
+            errorMess.text = "ファイル受信が完了しました: " + filePath;
+
+            // ソケットを閉じる
+            stream.Close();
+            client.Close();
+        }
+        catch (Exception e)
+        {
+            Debug.LogError("エラーが発生しました: " + e.Message);
+            //デバッグ後削除
+            errorMess.text = "エラーが発生しました: " + e.Message;
         }
     }
 }
